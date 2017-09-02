d364fcaeba6816a0910e3e6fa0eef68a5832b974
iGio90 <giovanni.rocca.90@gmail.com>
2017-09-03 00:18:27 +0200

    boom beach patcher + proxy

---
+++ src/com/scdevteam/proxies/bb/BoomBeachClient.java
@@ -0,0 +1,19 @@
+package com.scdevteam.proxies.bb;
+
+import com.scdevteam.proxies.BaseClient;
+
+public class BoomBeachClient extends BaseClient {
+    public BoomBeachClient(BoomBeachProxy proxy) {
+        super(proxy);
+    }
+
+    @Override
+    public String getKey() {
+        return "f6e07c02b728dfee9a849204b2ac1a3004ae3ce191092ae9ae5188db41768528";
+    }
+
+    @Override
+    public String getGameHost() {
+        return "game.boombeachgame.com";
+    }
+}
